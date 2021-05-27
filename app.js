51fc220ae78cb1c4f5acefb0ae940c363a3f5703
khongtien21 <khongtien.contact@gmail.com>
2021-05-27 15:13:43 +0700

    Khởi tạo web

---
+++ app.js
@@ -0,0 +1,17 @@
+// == <= >= === => --> <- != !==
+const student = 'Khổng Tiến';
+
+console.log(student);
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+if(4 !== 5) {
+  console.log('true');
+} 
+
+/**
+ * Không biết mình đang học j luôn đấy
+ * 
+ */
